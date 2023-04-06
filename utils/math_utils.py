6feac08d5178e1e3d696b54803c041a649bd2226
Keith Mills <kgmills@ualberta.ca>
2023-04-06 17:35:43 -0600

    Add a utility function

--- utils/math_utils.py
+++ utils/math_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def mean(list_val, fallback_val=None):
     if len(list_val) == 0:
         if fallback_val is not None:
@@ -17,3 +18,18 @@ def variance(list_val, fallback_val=0):
             raise ValueError()
     v = np.var(np.asarray(list_val))
     return v
+
+
+def make_divisible(v, divisor, min_val=None):
+    """
+    This function is taken from the original tf repo.
+    It can be seen here:
+    https://github.com/tensorflow/models/blob/master/research/slim/nets/mobilenet/mobilenet.py
+    """
+    if min_val is None:
+        min_val = divisor
+    new_v = max(min_val, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
